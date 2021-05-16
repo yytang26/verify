7a853fec39d438743b6504dda779fb5c0db5fb73
yytang26 <yytangtangyy@outlook.com>
2021-05-16 14:58:35 +0800

    配置和sql以及属性

--- src/main/sql/20210515.sql
+++ src/main/sql/20210515.sql
@@ -12,3 +12,20 @@ create table `Student` (
     `address` varchar (32) DEFAULT NULL COMMENT '地址',
     `sex` int (2) NOT NULL DEFAULT '0' COMMENT '性别'
 )
+
+create table `user_operation` (
+    `id` bigint(16)  unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+    `action` varchar (255) DEFAULT NULL  comment '行为',
+     `method` varchar (255) default null comment '方法',
+     `params` varchar(255)  default ''  comment '参数',
+    `remark` varchar (255) DEFAULT NULL  comment '标记',
+     `ip` varchar (255) default NULL comment 'ip地址',
+    `create_time` datetime not null DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+    `update_time` datetime not null DEFAULT CURRENT_TIMESTAMP ON UPDATE  CURRENT_TIMESTAMP COMMENT '更新时间',
+    `creator_id` varchar(32) DEFAULT NULL COMMENT '创建人名称',
+    `operator_id` varchar (32) DEFAULT NULL COMMENT '修改人id',
+    `creator` varchar(32) DEFAULT NULL COMMENT '创建人',
+    `operator` varchar (32) DEFAULT NULL COMMENT '修改人',
+    `is_delete` int(2) not null DEFAULT 0 comment '逻辑删除',
+    PRIMARY key (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT '用户操作记录表'
